7cc4f3d6100dbf99c861adaec58fd679c4c75b3e
Joseph Metran <josh_metran@yahoo.com>
2025-06-20 21:34:33 +0800

    order details validations

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,14 @@
 class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :product
+
+  validates :product_id, :order_id, presence: true
+  validates :qty, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :price, presence: true, numericality: { :only_integer => false }  
+
+  before_save :compute_total_price
+
+  def compute_total_price
+    self.total_price = qty * price
+  end
 end
