d51b3a101896dfafbb31f29d2b37a6c9b373377d
Joseph Metran <josh_metran@yahoo.com>
2025-06-20 21:33:40 +0800

    order validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,18 @@
 class Order < ApplicationRecord
   has_many :order_details, dependent: :destroy
   has_many :products, through: :order_details
+
+  validates :total_price, presence: true
+
+  validates :total_price,
+    numericality: { :only_integer => false },
+    if: :total_price?
+
+  validate :must_have_order_detail
+
+  def must_have_order_detail
+    if order_details.empty? || order_details.all?(&:marked_for_destruction?)
+      errors.add(:base, "Order must have at least one order detail")
+    end
+  end
 end
