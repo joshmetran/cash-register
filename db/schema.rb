58755a72a1ad1b29022674c200640cab80d2025e
Joseph Metran <josh_metran@yahoo.com>
2025-06-20 19:41:25 +0800

    added seed data for product

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_20_112012) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "order_details", force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.bigint "product_id", null: false
+    t.decimal "price", precision: 16, scale: 2, default: "0.0"
+    t.integer "qty"
+    t.decimal "total_price", precision: 16, scale: 2, default: "0.0"
+    t.string "remarks"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_details_on_order_id"
+    t.index ["product_id"], name: "index_order_details_on_product_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "invoice_no"
+    t.decimal "total_price", precision: 16, scale: 2, default: "0.0"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "code"
+    t.string "name"
+    t.decimal "price", precision: 16, scale: 2, default: "0.0"
+    t.boolean "is_buy_one_take_one"
+    t.boolean "can_bulk_purchase"
+    t.integer "min_bulk_purchase"
+    t.decimal "bulk_purchase_price", precision: 16, scale: 2, default: "0.0"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "order_details", "orders"
+  add_foreign_key "order_details", "products"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+Product.create([
+  { code: 'GR1', name: 'Green Tea', price: 3.11, is_buy_one_take_one: true, can_bulk_purchase: false, min_bulk_purchase: 0, bulk_purchase_price: 0 },
+  { code: 'SR1', name: 'Strawberries', price: 5.00, is_buy_one_take_one: false, can_bulk_purchase: true, min_bulk_purchase: 3, bulk_purchase_price: 4.50 },
+  { code: 'CF1', name: 'Coffee', price: 11.23, is_buy_one_take_one: false, can_bulk_purchase: true, min_bulk_purchase: 3, bulk_purchase_price: 7.49 }
+])
