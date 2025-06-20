1222b205b453c6dd6510b51e20e340030196dac9
Joseph Metran <josh_metran@yahoo.com>
2025-06-20 19:22:36 +0800

    added decimal precision

--- db/migrate/20250620110119_create_products.rb
+++ db/migrate/20250620110119_create_products.rb
@@ -3,11 +3,11 @@ class CreateProducts < ActiveRecord::Migration[8.0]
     create_table :products do |t|
       t.string :code
       t.string :name
-      t.decimal :price
+      t.decimal :price, precision: 16, scale: 2, default: 0
       t.boolean :is_buy_one_take_one
       t.boolean :can_bulk_purchase
       t.integer :min_bulk_purchase
-      t.decimal :bulk_purchase_price
+      t.decimal :bulk_purchase_price, precision: 16, scale: 2, default: 0
 
       t.timestamps
     end
