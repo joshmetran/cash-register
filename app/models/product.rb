4e5d3ad33c1c608fe03c9f45e8f4b427d7554b89
Joseph Metran <josh_metran@yahoo.com>
2025-06-20 21:34:07 +0800

    product data validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,18 @@
 class Product < ApplicationRecord
   has_many :order_details
   has_many :orders, through: :order_details  
+
+  validates :code, :name, :price, presence: true
+
+  validates :price,
+    numericality: { :only_integer => false },
+    if: :price?
+
+  validates :min_bulk_purchase,
+    numericality: { :only_integer => true, greater_than: 0 },
+    if: :can_bulk_purchase?
+  
+  validates :bulk_purchase_price,
+    numericality: { :only_integer => false, greater_than: 0 },
+    if: :can_bulk_purchase?
 end
