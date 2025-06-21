82069bd97cc354c28f6d3c16e75dfb4f6c3ded32
Joseph Metran <josh_metran@yahoo.com>
2025-06-21 22:57:22 +0800

    product filters and validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,8 @@ class Product < ApplicationRecord
   has_many :orders, through: :order_details  
 
   validates :code, :name, :price, presence: true
+  validates :code, uniqueness: { case_sensitive: false }, if: -> { self.code.present? }
+  validates :name, uniqueness: { case_sensitive: false }, if: -> { self.name.present? }
 
   validates :price,
     numericality: { :only_integer => false },
@@ -15,6 +17,14 @@ class Product < ApplicationRecord
   validates :bulk_purchase_price,
     numericality: { :only_integer => false, greater_than: 0 },
     if: :can_bulk_purchase?
+
+  scope :search, ->(keyword) {
+    where("code ILIKE :keyword OR name ILIKE :keyword", keyword: "%#{keyword}%")
+  }
+
+  scope :sortBy, ->(sort_by_key, sort_by_order) {
+    order(sort_by_key => sort_by_order)
+  }
 
   def has_bulk_purchased_discount?(qty)
     can_bulk_purchase? && qty >= min_bulk_purchase
